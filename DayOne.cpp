69b9dfc80d819fba71dfa8aec4297d9cca7e2a57
Kr4ken-9 <Kr4kenKT@gmail.com>
2017-12-04 18:41:10 -0600

    Day One Solved

---
+++ DayOne.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int Captcha(const std::string Input)
+{
+    int Output = 0;
+    int Length = Input.length();
+    int ArrayLength = Length - 1;
+
+    for(int i = 0; i < Length; i++)
+    {
+        if(i == ArrayLength)
+        {
+            if (Input[i] == Input[0])
+                Output += stoi(string(1, Input[i]));
+
+            break;
+        }
+
+        if(Input[i] == Input[i + 1])
+            Output += stoi(string(1, Input[i]));
+    }
+
+    return Output;
+}
