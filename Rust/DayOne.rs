f6e2b0759a7ec49c8b8a40ffa0ce3dc4310d5f65
Kr4ken-9 <Kr4kenKT@gmail.com>
2017-12-09 14:24:26 -0600

    Day One Pt 2 Rust

--- Rust/DayOne.rs
+++ Rust/DayOne.rs
@@ -1,5 +1,4 @@
 pub fn Captcha(input: &str) -> u32 {
-    let length = input.len();
     let mut first_int: u32 = 0;
     let mut last_int: u32 = 0;
     let mut output = 0;
@@ -13,7 +12,7 @@ pub fn Captcha(input: &str) -> u32 {
             continue;
         }
 
-        if index == length - 1 {
+        if index == input.len() - 1 {
             if int == first_int {
                 output += int;
             }
@@ -24,6 +23,25 @@ pub fn Captcha(input: &str) -> u32 {
         }
 
         last_int = int;
+    }
+
+    return output;
+}
+
+pub fn CaptchaPtTwo(input: &str) -> u32 {
+    let mut output = 0;
+    let offset = input.len() / 2;
+
+    for (index, character) in input.chars().enumerate() {
+        let mut next_index = index + offset;
+
+        if next_index >= input.len() {
+            next_index = next_index - input.len()
+        }
+
+        if character == input.chars().nth(next_index).unwrap() {
+            output += character.to_digit(10).unwrap();
+        }
     }
 
     return output;
