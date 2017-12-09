7756bde3fffb5cdfb23cee433a7f7c6c9733eab8
Unknown <Kr4kenKT@gmail.com>
2017-12-09 11:32:51 -0600

    Day One Pt 2 C++

--- C++/DayOne.cpp
+++ C++/DayOne.cpp
@@ -3,12 +3,10 @@
 int Captcha(const std::string Input)
 {
     int Output = 0;
-    int Length = Input.length();
-    int ArrayLength = Length - 1;
 
-    for(int i = 0; i < Length; i++)
+    for(int i = 0; i < Input.length(); i++)
     {
-        if(i == ArrayLength)
+        if(i == Input.length() - 1)
         {
             if (Input[i] == Input[0])
                 Output += stoi(string(1, Input[i]));
@@ -17,6 +15,25 @@ int Captcha(const std::string Input)
         }
 
         if(Input[i] == Input[i + 1])
+            Output += stoi(string(1, Input[i]));
+    }
+
+    return Output;
+}
+
+int CaptchaPtTwo(const std::string Input)
+{
+    int Output = 0;
+    const int Offset = Input.length() / 2;
+
+    for(int i = 0; i < Input.length(); i++)
+    {
+        int Index = i + Offset;
+
+        if(Index >= Input.length())
+            Index = Index - Input.length();
+
+        if(Input[i] == Input[Index])
             Output += stoi(string(1, Input[i]));
     }
 
