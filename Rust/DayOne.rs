855d05398c0b7ab25453033a831184802da5868e
Kr4ken-9 <Kr4kenKT@gmail.com>
2017-12-09 14:29:10 -0600

    Clean Up Day One Rust

--- Rust/DayOne.rs
+++ Rust/DayOne.rs
@@ -1,28 +1,19 @@
 pub fn Captcha(input: &str) -> u32 {
-    let mut first_int: u32 = 0;
-    let mut last_int: u32 = 0;
     let mut output = 0;
 
     for (index, character) in input.chars().enumerate() {
         let int: u32 = character.to_digit(10).unwrap();
         
-        if index == 0 {
-            first_int = int;
-            last_int = int;
-            continue;
-        }
-
         if index == input.len() - 1 {
-            if int == first_int {
+            if character == input.chars().nth(0).unwrap() {
                 output += int;
             }
+            break;
         }
 
-        if int == last_int {
+        if character == input.chars().nth(index + 1).unwrap() {
             output += int;
         }
-
-        last_int = int;
     }
 
     return output;
